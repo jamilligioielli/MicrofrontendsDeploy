8b0cda33fc50ce4ee33d5b9701d1b7dac1f8f7c6
jamilligioielli <jamilligioielli@outlook.com>
2021-12-17 17:03:58 -0300

    Added dashboard app

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -1,30 +1,24 @@
-const { merge } = require('webpack-merge')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const packageJson = require('../package.json')
-const commonConfig = require('./webpack.common')
-
-// this will contain a string that says exactly
-// where the production app wil be hosted
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
-	mode: 'production',
-	// all the different files will use this as a template
-	// on how to name them
-	output: {
-		filename: '[name].[contenthash].js',
-		publicPath: '/dashboard/latest/'
-	},
-	plugins: [
-		// those plugins will now have a production domain
-		new ModuleFederationPlugin({
-			name: 'dashboard',
-			filename: 'remoteEntry.js',
-			exposes: {
-				'./DashboardApp': './src/bootstrap'
-			},
-			shared: packageJson.dependencies
-		})
-	]
-}
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/dashboard/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
-module.exports = merge(commonConfig, prodConfig)
+module.exports = merge(commonConfig, prodConfig);
